638700788fa3a4b38f4ec47625f6d26960d5e078
pourkarim1385 <46225551+pourkarim1385@users.noreply.github.com>
2025-12-12 01:17:54 +0330

    main.c

--- main.c
+++ main.c
@@ -7,9 +7,12 @@
 #define SUCCESS 1
 #define TIME 2
 
+/*Function Definitions*/
 int check(int[][3], int);
+int trend(int, int);
 void printResult(int);
 
+/*main function*/
 int main(void) {
     /*Variable Definition*/
     int n = 0;
@@ -30,44 +33,46 @@ int main(void) {
     return 0;
 }
 
+/*Function Declaration*/
+
 /*check: return the status of the athlete*/
 int check(int records[][3], int counter) {
-    int avg_difficulty = 0;
+    /*calculating*/
+    int avg_difficulty = records[0][DIFFICULTY];
     int running_time_rate = 0, success_rate = 0;
-
     for (int i = 1; i < counter; i++) {
-        if (records[i][TIME] > records[i-1][TIME]) {
-            running_time_rate = IMPROVING;
-        }
-        else if (records[i][TIME] < records[i-1][TIME]) {
-            running_time_rate = DECLINING;
-        }
-        else {
-            running_time_rate = STABLE;
-        }
-
-        if (records[i][SUCCESS] > records[i-1][SUCCESS]) {
-            success_rate = IMPROVING;
-        }
-        else if (records[i][SUCCESS] < records[i-1][SUCCESS]) {
-            success_rate = DECLINING;
-        }
-        else {
-            success_rate = STABLE;
-        }
+        running_time_rate = trend(records[i-1][TIME], records[i][TIME]);
+        success_rate = trend(records[i-1][SUCCESS], records[i][SUCCESS]);
 
         avg_difficulty += records[i][DIFFICULTY];
     }
     avg_difficulty = (avg_difficulty / counter);
 
-    if ((running_time_rate == SUCCESS || running_time_rate == STABLE) && (success_rate == SUCCESS || success_rate == STABLE) && avg_difficulty >= 5) {
+    /*defining the situations*/
+    int time_improvement = (running_time_rate == SUCCESS || running_time_rate == STABLE);
+    int time_decline = (running_time_rate == DECLINING || running_time_rate == STABLE);
+
+    int succes_improvement = (success_rate == SUCCESS || success_rate == STABLE);
+    int succes_decline = (success_rate == DECLINING || success_rate == STABLE);
+
+    /*validating and returning*/
+    if (time_improvement && succes_improvement && avg_difficulty >= 5) {
         return IMPROVING;
     }
-    if ((running_time_rate == DECLINING || running_time_rate == STABLE) && (success_rate == DECLINING || success_rate == STABLE) && avg_difficulty < 5) {
+    if (time_decline && succes_decline && avg_difficulty < 5) {
         return DECLINING;
     }
     return STABLE;
+}
 
+/*trend
+ value a: previous value , value b: new value
+ returns the trend status.
+ */
+int trend(int valueA, int valueB) {
+    if (valueB > valueA) return IMPROVING;
+    if (valueB < valueA) return DECLINING;
+    return STABLE;
 }
 
 /*printResult does the print job*/
